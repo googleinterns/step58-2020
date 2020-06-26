4d61495a036b4abb4798945b0387105500e2d433
Ife Omidiran <omidiran@college.harvard.edu>
2020-06-26 14:55:31 -0500

    Add cyclomatic complexity analysis for sample code

--- scripts/solve.js
+++ scripts/solve.js
@@ -1,7 +1,15 @@
+const sampleCode = 'function myScript(){return 100;}\n';
 window.addEventListener('load', function() {
   var myCodeMirror = CodeMirror(document.getElementById('code-area'), {
-    value: "function myScript(){return 100;}\n",
-    mode:  "javascript",
+    value: sampleCode,
+    mode: 'javascript',
     lineNumbers: true
   });
+
+  JSHINT(sampleCode);
+  const results = JSHINT.data();
+  let totalComplexity = 0;
+  results.functions.forEach(fn => totalComplexity += fn.metrics.complexity);
+  document.getElementById('analysis-output').innerText = 'The cyclomatic complexity is ' + totalComplexity;
+  console.log(JSHINT.data());
 });
