ee213303b33cbf86c228f19073bd722e8f5fd7ad
Nicholas Husin <nicholashusin@gmail.com>
2020-06-26 20:42:31 +0000

    Add documentations to solve.js

--- scripts/solve.js
+++ scripts/solve.js
@@ -11,12 +11,15 @@ var runButton;
 var stopButton;
 var keepRunningCode;
 
-
 window.addEventListener('load', function() {
   setupCodeMirror();
   setupElements();
 });
 
+/**
+ * Sets up text editor.
+ * Further modification to the text editor should be done here.
+ **/
 function setupCodeMirror() {
   codeMirror = CodeMirror(document.getElementById(CODE_AREA_ID), {
     value: 'function myScript(){return 100;}\n',
@@ -31,6 +34,13 @@ function setupElements() {
   stopButton    = document.getElementById(STOP_BUTTON_ID);
 }
 
+/**
+* Function to override functions call inside the sandbox interpreter.
+* This function serves as an interface between the browser and the sandbox.
+* 
+* By overriding alert() in the sandbox for example, we can modify alert()
+* so that its can be displayed on the GUI, outside of the sandbox.
+**/
 function overrideFunctions(interpreter, scope) {
   var alertOverride = function(text) {
     outputArea.innerHTML += text + NEWLINE;
@@ -50,6 +60,15 @@ function overrideFunctions(interpreter, scope) {
     interpreter.createNativeFunction(assertOverride));
 }
 
+/**
+* Wrapper function around startRunningCode.
+* This function mostly serves to try to catch any error encountered
+* when trying to interpret invalid JavaScript code,
+* preventing the interpreter from failing silently.
+*
+* Should an invalid JavaScript code be encountered, it will be
+* displayed to the user.
+**/
 function executeCode() {
   outputArea.innerHTML  = '';
   keepRunningCode       = true;
@@ -76,6 +95,12 @@ function updateButtons() {
   }
 }
 
+/**
+* Runs the interpreter step by step until we are finished.
+* Utilizes setTimeout() to allow other tasks to run too
+* and prevents the application from bricking in cases such as
+* infinite loops.
+**/
 function startRunningCode(interpreter) {
   function nextStep() {
     if (keepRunningCode && interpreter.step()) {
