66d885c119c2ee862680f7828abd3eb8596639f4
Ife Omidiran <omidiran@college.harvard.edu>
2020-06-26 16:40:11 -0500

    Refactor analysis to separate function

--- scripts/solve.js
+++ scripts/solve.js
@@ -1,14 +1,19 @@
 const sampleCode = 'function myScript(){return 100;}\n';
+
 window.addEventListener('load', function() {
-  var myCodeMirror = CodeMirror(document.getElementById('code-area'), {
+  const myCodeMirror = CodeMirror(document.getElementById('code-area'), {
     value: sampleCode,
     mode: 'javascript',
     lineNumbers: true
   });
 
-  JSHINT(sampleCode);
-  const results = JSHINT.data();
+  runStaticAnalysis(myCodeMirror.getValue());
+});
+
+function runStaticAnalysis(code) {
   let totalComplexity = 0;
+  JSHINT(code);
+  const results = JSHINT.data();
   results.functions.forEach(fn => totalComplexity += fn.metrics.complexity);
   document.getElementById('analysis-output').innerText = 'The cyclomatic complexity is ' + totalComplexity;
-});
+}
