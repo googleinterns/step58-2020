84977af9871557c48df2e84f8d72deebef667c1b
Nicholas Husin <nicholashusin@gmail.com>
2020-06-26 20:16:10 +0000

    Add basic running and stopping functionality

--- scripts/solve.js
+++ scripts/solve.js
@@ -1,21 +1,38 @@
 const SAMPLE_CODE       = 'function myScript(){return 100;}\n';
 const CODE_AREA_ID      = 'code-area';
 const CODE_OUTPUT_ID    = 'code-output';
+const RUN_BUTTON_ID     = 'run-button';
+const STOP_BUTTON_ID    = 'stop-button';
+const HIDDEN_ATTRIBUTE  = 'hidden';
 const NEWLINE           = '\n';
 
 var codeMirror;
 var outputArea;
+var runButton;
+var stopButton;
+var keepRunningCode;
 
 window.addEventListener('load', function() {
+  setupCodeMirror();
+  setupElements();
+  runStaticAnalysis(myCodeMirror.getValue());
+});
+
+function setupCodeMirror() {
   codeMirror = CodeMirror(document.getElementById(CODE_AREA_ID), {
     value: 'function myScript(){return 100;}\n',
     mode:  'javascript',
     lineNumbers: true,
   });
+}
 
-  outputArea = document.getElementById(CODE_OUTPUT_ID);
-  runStaticAnalysis(myCodeMirror.getValue());
 });
+
+function setupElements() {
+  outputArea    = document.getElementById(CODE_OUTPUT_ID);
+  runButton     = document.getElementById(RUN_BUTTON_ID);
+  stopButton    = document.getElementById(STOP_BUTTON_ID);
+}
 
 function overrideFunctions(interpreter, scope) {
   var alertOverride = function(text) {
@@ -28,9 +45,40 @@ function overrideFunctions(interpreter, scope) {
 
 function executeCode() {
   let code      = codeMirror.getValue();
-  myInterpreter = new Interpreter(code, overrideFunctions);
+  interpreter = new Interpreter(code, overrideFunctions);
 
-  myInterpreter.run();
+  startRunningCode(interpreter);
+}
+
+function updateButtons() {
+  if (keepRunningCode) {
+    runButton.setAttribute(HIDDEN_ATTRIBUTE, true);
+    stopButton.removeAttribute(HIDDEN_ATTRIBUTE);
+  } else {
+    stopButton.setAttribute(HIDDEN_ATTRIBUTE, true);
+    runButton.removeAttribute(HIDDEN_ATTRIBUTE);
+  }
+}
+
+function startRunningCode(interpreter) {
+  outputArea.innerHTML  = '';
+  keepRunningCode       = true;
+  updateButtons();
+
+  function nextStep() {
+    if (keepRunningCode && interpreter.step()) {
+      setTimeout(nextStep, 0);
+    } else {
+      keepRunningCode = false;
+      updateButtons();
+    }
+  }
+  nextStep();
+}
+
+function stopRunningCode() {
+  keepRunningCode = false;
+  updateButtons();
 }
 
 function runStaticAnalysis(code) {
