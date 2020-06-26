1074c61b9762c8bbe6a34f670879ace123ad0cad
Nicholas Husin <nicholashusin@gmail.com>
2020-06-26 18:21:36 +0000

    Add basic execute code functionality with alert override

--- scripts/solve.js
+++ scripts/solve.js
@@ -1,14 +1,37 @@
-const sampleCode = 'function myScript(){return 100;}\n';
+const SAMPLE_CODE       = 'function myScript(){return 100;}\n';
+const CODE_AREA_ID      = 'code-area';
+const CODE_OUTPUT_ID    = 'code-output';
+const NEWLINE           = '\n';
+
+var codeMirror;
+var outputArea;
 
 window.addEventListener('load', function() {
-  const myCodeMirror = CodeMirror(document.getElementById('code-area'), {
-    value: sampleCode,
-    mode: 'javascript',
-    lineNumbers: true
+  codeMirror = CodeMirror(document.getElementById(CODE_AREA_ID), {
+    value: 'function myScript(){return 100;}\n',
+    mode:  'javascript',
+    lineNumbers: true,
   });
 
+  outputArea = document.getElementById(CODE_OUTPUT_ID);
   runStaticAnalysis(myCodeMirror.getValue());
 });
+
+function overrideFunctions(interpreter, scope) {
+  var alertOverride = function(text) {
+    outputArea.innerHTML += text + NEWLINE;
+  };
+
+  interpreter.setProperty(scope, 'alert', 
+    interpreter.createNativeFunction(alertOverride));
+}
+
+function executeCode() {
+  let code      = codeMirror.getValue();
+  myInterpreter = new Interpreter(code, overrideFunctions);
+
+  myInterpreter.run();
+}
 
 function runStaticAnalysis(code) {
   let totalComplexity = 0;
