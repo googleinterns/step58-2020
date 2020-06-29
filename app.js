95111914fd4e3d3f6e7826f9171df53f62e4b9ad
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-06-29 16:04:35 +0000

    Changed the NAV bar titles

--- app.js
+++ app.js
@@ -16,8 +16,13 @@ app.get('/solve', function(request, response){
   response.sendFile('html/solve.html', { root: __dirname });
 });
 
+//This is for the home page
 app.get('/', function(request, response){
   response.sendFile('html/home.html', { root: __dirname });
+});
+//This is for the sign in page
+app.get('/sign-in', function(request, response){
+  response.sendFile('html/sign-in.html', { root: __dirname });
 });
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`))
