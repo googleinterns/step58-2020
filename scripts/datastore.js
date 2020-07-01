ba7080d56c80c6cbcfaf8eb27990a2e0b0436dc9
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-01 15:13:36 +0000

    Example of User Datasore

---
+++ scripts/datastore.js
@@ -0,0 +1,34 @@
+// Imports the Google Cloud client library
+const Datastore = require('@google-cloud/datastore');
+
+const projectId = 'cloud-ad-step-2020';
+
+// Creates a client
+const datastore = new Datastore({
+  projectId: projectId,
+});
+
+// The kind for the new entity
+const kind = 'User';
+// The name/ID for the new entity
+const name = 'sampleUser1';
+// The Cloud Datastore key for the new entity
+const userKey = datastore.key([kind, name]);
+
+// Prepares the new entity
+const user = {
+  key: userKey,
+  data: {
+    description: 'ID Key',
+  },
+};
+
+// Saves the entity
+datastore
+  .save(user)
+  .then(() => {
+    console.log(`Saved ${user.key.name}: ${user.data.description}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
