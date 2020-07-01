58f1b45bea2c6d67448aa29192b6efb555e54d80
Nicholas Husin <nicholashusin@gmail.com>
2020-07-01 20:21:28 +0000

    Modify sandbox test to be asynchronous

--- test/code_sandbox.test.js
+++ test/code_sandbox.test.js
@@ -1,32 +1,32 @@
 const sandbox = require('../modules/code_sandbox_manager.js');
 const assert = require('assert');
 
-describe('Code Sandbox', function () {
-  describe('alert()', function () {
-    it(`should be connected to sandbox host's stdout`, function () {
+describe('Code Sandbox', async() => {
+  describe('alert()', async() => {
+    it(`should be connected to sandbox host's stdout`, async() => {
       const code      = `alert('Hello there!');`;
-      const result    = sandbox.run(code);
+      const result    = await sandbox.run(code);
 
       assert.equal(result.stdout, 'Hello there!\n');
     });
   });
 
-  describe('alert()', function () {
-    it(`should be connected to sandbox host's stderr`, function () {
+  describe('alert()', async() => {
+    it(`should be connected to sandbox host's stderr`, async() => {
       const code      = `assert(false);`;
-      const result    = sandbox.run(code);
+      const result    = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('Assertion failed\n'));
     });
 
-    it(`should be able to display custom message`, function () {
+    it(`should be able to display custom message`, async() => {
       const code      = `assert(false, 'Something went wrong');`;
-      const result    = sandbox.run(code);
+      const result    = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('Something went wrong\n'));
     });
 
-    it(`should stop program on first false assertion`, function () {
+    it(`should stop program on first false assertion`, async() => {
       const code = `
       alert(1);
       assert(true);
@@ -35,7 +35,7 @@ describe('Code Sandbox', function () {
       alert(3);
       `;
 
-      const result = sandbox.run(code);
+      const result = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('Assertion failed\n'));
       assert.equal(result.stdout, '1\n2\n');
@@ -43,8 +43,8 @@ describe('Code Sandbox', function () {
   });
 
 
-  describe('interpreter', function () {
-    it(`should detect invalid JavaScript syntax`, function () {
+  describe('interpreter', async() => {
+    it(`should detect invalid JavaScript syntax`, async() => {
       const code = `
       package main
       import "fmt"
@@ -54,37 +54,37 @@ describe('Code Sandbox', function () {
       }
       `;
 
-      const result = sandbox.run(code);
+      const result = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('SyntaxError: Unexpected token (2:14)'));
     });
 
-    it(`should detect undeclared function reference`, function () {
+    it(`should detect undeclared function reference`, async() => {
       const code = `alert(go());`;
-      const result = sandbox.run(code);
+      const result = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('ReferenceError: go is not defined'));
     });
 
-    it(`should detect undeclared variable reference`, function () {
+    it(`should detect undeclared variable reference`, async() => {
       const code = `alert(str);`;
-      const result = sandbox.run(code);
+      const result = await sandbox.run(code);
 
       assert.ok(result.stderr.includes('ReferenceError: str is not defined'));
     });
   });
 
-  describe('timeout', function () {
-    it(`should prevent infinite loop given a timeout argument`, function () {
+  describe('timeout', async() => {
+    it(`should prevent infinite loop given a timeout argument`, async() => {
       const code = `while(true);`;
-      const result = sandbox.run(code, 100);
+      const result = await sandbox.run(code, 100);
 
       assert.equal(result.signal, 'SIGTERM');
     });
 
-    it(`should use default timeout when not given an explicit argument`, function () {
+    it(`should use default timeout when not given an explicit argument`, async() => {
       const code = `while(true);`;
-      const result = sandbox.run(code);
+      const result = await sandbox.run(code);
 
       assert.equal(result.signal, 'SIGTERM');
     });
