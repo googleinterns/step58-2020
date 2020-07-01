708263ff6c00a956e62d010119ab28b452827f99
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-01 15:37:23 +0000

    created a modules directory and moved the datastore.js file to that directory

--- modules/datastore.js
+++ modules/datastore.js
@@ -1,5 +1,5 @@
 // Imports the Google Cloud client library
-const {Datastore} = require('@google-cloud/datastore');
+const Datastore = require('@google-cloud/datastore');
 
 const projectId = 'cloud-ad-step-2020';
 
@@ -8,57 +8,29 @@ const datastore = new Datastore({
   projectId: projectId,
 });
 
-//This is the function for saving user's data
-async function userData(){
-    //The kind for the new entity being the user info
-    const kind = 'User';
-    //The name that the username for the new entity
-    const name = 'sampleUser1';
-    //The Cloud Datastore key for the new entity
-    const userKey = datastore.key([kind, name]);
+// The kind for the new entity
+const kind = 'User';
+// The name for the new entity
+const name = 'sampleUser1';
+//The email for the new entity
+const email = 'user@gmail.com'
+// The Cloud Datastore key for the new entity
+const userKey = datastore.key([kind, name, email]);
 
-    //Preparing the new entity
-    const user = {
-        key: userKey,
-        data: {
-            description: 'User Key',
-            email: 'user@gmail.com'
-        },
-    };
+// Prepares the new entity
+const user = {
+  key: userKey,
+  data: {
+    description: 'ID Key',
+  },
+};
 
-    //Saving the user entity
-    datastore
-        .save(user)
-        .then(() => {
-            console.log(`Saved ${user.key.name}: ${user.data.description}, ${user.data.email}`);
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
-}
-userData();
-
-
-/*async function problemData() {
-  // The kind for the new entity
-  const kind = 'Task';
-
-  // The name/ID for the new entity
-  const name = 'sampletask1';
-
-  // The Cloud Datastore key for the new entity
-  const taskKey = datastore.key([kind, name]);
-
-  // Prepares the new entity
-  const task = {
-    key: taskKey,
-    data: {
-      description: 'Buy milk',
-    },
-  };
-
-  // Saves the entity
-  await datastore.save(task);
-  console.log(`Saved ${task.key.name}: ${task.data.description}`);
-}
-problemData();*/
+// Saves the entity
+datastore
+  .save(user)
+  .then(() => {
+    console.log(`Saved ${user.key.name}: ${user.data.description}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
