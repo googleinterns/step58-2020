a719f34f238728eb5c088852766021cb0dd2ec89
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-06 21:51:18 +0000

    Everything needed for User's information to be stored is completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path      = require('path');
 const express   = require('express');
 const exphbs    = require('express-handlebars');
 const app       = express();
+const userData  = require('./modules/datastore.js');
 
 /**
 * Setup express handlebars as our templating engine.
@@ -21,6 +22,16 @@ app.set('view engine', 'hbs');
 app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
 app.use('/stylesheets', express.static(path.join(__dirname, 'stylesheets')));
 app.use('/lib', express.static(path.join(__dirname, 'lib')));
+
+//Handeling GET and POST request in Express
+app.post('/addUser',(request,response) => {
+    //code to perform particular action.
+    console.log(request.query.name);
+    console.log(request.query.email);
+    userData(request.query.name, request.query.email);
+});
+
+//app.post('/storeSubmission')
 
 /**
 * Responds to requests for routes by giving html files with the same name if possible.
--- scripts/auth.js
+++ scripts/auth.js
@@ -2,23 +2,11 @@
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     const profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to server!
-    console.log('Full Name: ' + profile.getName());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    const id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-
-    //Sending the ID to the server with an HTTPS POST request
-    const xhr = new XMLHttpRequest();
-    //xhr.open('POST', 'https://cloud-ad-step-2020.uc.r.appspot.com');
-    xhr.open('POST', 'https://8080-f7dcdea0-3cc5-4401-86b4-0bac092a4a2e.us-central1.cloudshell.dev/')
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function() {
-    console.log('Signed in as: ' + xhr.responseText);
-    };
-    xhr.send('idtoken=' + id_token);
+    const name = profile.getName();
+    const email = profile.getEmail();
+    //To send the information to the server
+    const request = new Request("/addUser?name=" + name + "&email=" + email, {method: "POST"}); 
+    fetch(request);
 }
 
 function signOut(){
