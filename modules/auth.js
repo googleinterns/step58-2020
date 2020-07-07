9cfdcf38bba247a25919e8119cd8372b8308ff32
Ife Omidiran <omidiran@college.harvard.edu>
2020-07-07 15:03:58 -0500

    Restrict scope of user variable

--- modules/auth.js
+++ modules/auth.js
@@ -13,37 +13,21 @@ class User {
   }
 }
 
-let user;
-
 /**
- * Helper function to check if an ID token is verified and create a new user object if so.
+ * Verifies if an ID token and returns User object if the token is valid.
  * @param {string} token 
- * @return {Promise} Returns Promise resolving to a boolean.
+ * @return {Promise} Returns Promise resolving to a User object or null.
  */
-async function verify(token) {
+async function getUser(token) {
   try {
     const ticket = await client.verifyIdToken({
       idToken: token
     });
     const payload = ticket.getPayload();
-    user = new User(payload['name'], payload['email']); 
-    return true;
+    return new User(payload['name'], payload['email']); 
   } catch(error) {
     // The API returns one of various errors indicating 
     // that the token is invalid in some way
-    return false;
-  }
-}
-
-/**
- * @param {string} token 
- * @returns {Promise} Returns Promise resolving to a User object
- */
-async function getUser(token) {
-  const verified = await verify(token)
-  if (verified === true) {
-    return user;
-  } else {
-    throw 'ID Token is not valid.';
+    return null;
   }
 }
