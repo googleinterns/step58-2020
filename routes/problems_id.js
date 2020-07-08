18d2618e30d522488dd84e7565636f4c1d8dc1ab
Nicholas Husin <nicholashusin@gmail.com>
2020-07-08 21:59:20 +0000

    Implement dynamic problem display in /problems/:id

--- routes/problems_id.js
+++ routes/problems_id.js
@@ -1,9 +1,37 @@
+'use strict';
+
+const datastore     = require('../modules/datastore.js');
+const problemUtil   = require('../modules/problem_util.js');
+
+const PROBLEMS_KIND = 'Problem';
+
+problemUtil.addFromProblemsDir();
+
+/**
+ * Gets problem according to the id given.
+ * Utilizes parseInt() as integer sent through request
+ * may have its typing wrongly inferred by Node.
+ **/
+async function getProblem(id) {
+  const query = datastore
+    .createQuery(PROBLEMS_KIND)
+    .filter('id', '=', parseInt(id));
+
+  const [problems] = await datastore.runQuery(query);
+  return problems[0];
+}
+
 module.exports = function(app) {
-  app.get('/problems/:id', function (request, response) {
+  /**
+   * GET handler that displays problem dynamically according
+   * to the problem id given in the request parameter.
+   **/
+  app.get('/problems/:id', async function(request, response) {
+    const problem = await getProblem(request.params.id);
     let dynamicContent = {
-      'question-title'  : 'Placeholder title for /problems/' + request.params.id,
-      'question-text'   : 'Placeholder text for /problems/' + request.params.id,
-      'initial-code'    : `alert('Placeholder initial code for /problems/` + request.params.id + `')`
+      'question-title'  : problem.title,
+      'question-text'   : problem.text,
+      'initial-code'    : problem.code
     }
 
     response.render('solve', dynamicContent);
