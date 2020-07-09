d05aa3e62079a59e10621378336269978da3c36f
Nicholas Husin <nicholashusin@gmail.com>
2020-07-09 19:17:11 +0000

    Add auth token reset on sign out

--- scripts/auth.js
+++ scripts/auth.js
@@ -17,6 +17,6 @@ function onSignIn(googleUser) {
 function signOut(){
     const auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function(){
-        console.log('User signed out.');
+      idToken = null;
     });
 }
