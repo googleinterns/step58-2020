36959cbfb70a49b28cf400a0558ef851cb6f4d51
Nicholas Husin <nicholashusin@gmail.com>
2020-07-09 20:13:58 +0000

    Implement submission integration with Datastore
    - Allows one submission per user for each problem
    - Disallows submission if user is not logged in

--- routes/problems_id.js
+++ routes/problems_id.js
@@ -4,8 +4,10 @@ const datastore     = require('../modules/datastore.js');
 const problemUtil   = require('../modules/problem_util.js');
 const analyze       = require('../modules/code_metrics.js');
 const sandbox       = require('../modules/code_sandbox_manager.js');
+const auth          = require('../modules/auth.js');
 
 const PROBLEMS_KIND = 'Problem';
+const SOLUTION_KIND = 'Solution';
 
 problemUtil.addFromProblemsDir();
 
@@ -21,6 +23,36 @@ async function getProblem(id) {
 
   const [problems] = await datastore.runQuery(query);
   return problems[0];
+}
+
+/**
+ * Checks if a user has submitted an accepted solution before.
+ * This is done to prevent users from submitting other people's
+ * code they see after solving a problem for the first time,
+ * causing an influx in duplicate top answers.
+ **/
+async function hasSubmitted(user, problemId) {
+  const query = datastore
+    .createQuery(SOLUTION_KIND)
+    .filter('problemId', '=', parseInt(problemId))
+    .filter('email', '=', user.email);
+
+  const [problems] = await datastore.runQuery(query);
+  console.log(problems);
+  return problems.length != 0;
+}
+
+/**
+ * Helper function to save submission of solution.
+ * Fields in data are not nested to allow easier filtering.
+ **/
+async function saveSubmission(user, code, analysisResult, problemId) {
+  const data        = analysisResult;
+  data.email        = user.email;
+  data.code         = code;
+  data.problemId    = parseInt(problemId);
+
+  await datastore.store(SOLUTION_KIND, data);
 }
 
 module.exports = function(app) {
@@ -39,15 +71,31 @@ module.exports = function(app) {
     response.render('solve', dynamicContent);
   });
 
-  app.post('/problems/:id', function(request, response) {
-    /** TODO (b/160783943) integrate with datastore
-     * Test code by getting problem object from datastore and combine submitted source code with provided
-     * test cases. If there are no errors, store as new submission along with analysis and redirect 
-     * to updated rankings page. Otherwise, send response with failing test cases so client can alert user.
-     **/
-    
-    console.log(request.body.code);
-    console.log(analyze(request.body.code));
-    response.send(false);
+  app.post('/problems/:id', async function(request, response) {
+    const user  = await auth.getUser(request.body.authToken);
+    if (user == null) {
+      response.send('Not Authenticated');
+      return;
+    }
+
+    if (await hasSubmitted(user, request.params.id)) {
+      response.send('Multiple submissions are not allowed');
+      return;
+    }
+
+    const email = user.email;
+    const code  = request.body.code;
+    const tests = (await getProblem(request.params.id)).tests;
+
+    const analysisResult    = analyze(code);
+    const executionResult   = await sandbox.run(code + tests.join('\n'));
+
+    if (executionResult.stderr) {
+      response.send('Code did not pass all test cases');
+      return;
+    } 
+
+    await saveSubmission(user, code, analysisResult, request.params.id);
+    response.send('Your code passed all test cases!');
   });
 }
