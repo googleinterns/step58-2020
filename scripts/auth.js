cd7d28cd67061c65ac848206823bcd0d69631b8b
Nicholas Husin <nicholashusin@gmail.com>
2020-07-10 16:52:26 +0000

    Remove leftover unused content

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,19 +4,13 @@ export let idToken = null;
 window.onSignIn = onSignIn;
 window.signOut = signOut;
 
-//THIS IS JUST THE GOOGLE SIGN IN CODE
 function onSignIn(googleUser) {
-    //ID-TOKEN
     idToken = googleUser.getAuthResponse().id_token;
-    //To send the information to the server
-    const type = new Blob([JSON.stringify({authToken: idToken})], {type : 'application/json'});
-    //const request = new Request("/user", {method: "POST", body: JSON.stringify(user)}); 
-    fetch("/user", {method: "POST", body: type});
 }
  
 function signOut(){
     const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function(){
+    auth2.signOut().then(function() {
       idToken = null;
     });
 }
