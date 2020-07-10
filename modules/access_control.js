31172b56b11d18655c5eb9e5b035d271341ef313
Ife <omidiran@college.harvard.edu>
2020-07-10 22:59:58 +0000

    Add try catch block to handle query errors

--- modules/access_control.js
+++ modules/access_control.js
@@ -9,13 +9,17 @@ const SOLUTION_KIND = 'Solution';
  * for a given problemId.
  **/
 async function hasSubmission(user, problemId) {
-  const query = datastore
-    .createQuery(SOLUTION_KIND)
-    .filter('problemId', '=', parseInt(problemId))
-    .filter('email', '=', user.email);
+  try {
+    const query = datastore
+      .createQuery(SOLUTION_KIND)
+      .filter('problemId', '=', parseInt(problemId))
+      .filter('email', '=', user.email);
 
-  const [problems] = await datastore.runQuery(query);
-  return problems.length != 0;
+    const [problems] = await datastore.runQuery(query);
+    return problems.length != 0;
+  } catch(error) {
+    return false;
+  }
 }
 
 module.exports.hasSubmission = hasSubmission;
