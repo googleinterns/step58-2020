0d483dcd08a7cdc61375a3d2a96118cdfa87625f
Ife <omidiran@college.harvard.edu>
2020-07-14 20:15:32 +0000

    Fetch sign in and register status from server

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,13 +4,48 @@ export let idToken = null;
 window.onSignIn = onSignIn;
 window.signOut = signOut;
 
-function onSignIn(googleUser) {
-    idToken = googleUser.getAuthResponse().id_token;
+async function onSignIn(googleUser) {
+  idToken = googleUser.getAuthResponse().id_token;
+  const payload = new Blob([JSON.stringify({authToken: idToken})], {type: 'application/json'});
+
+  const response = await fetch('/sign-in', {method: 'POST', body: payload});
+
+  if (response.status !== 203) {
+    const user = await response.json();
+    document.getElementById('name').value = user.name;
+    document.getElementById('email').value = user.email;
+    document.getElementById('registerForm').addEventListener('submit', formSubmit);
+    $('#registerModal').modal('show');
+  }
 }
  
-function signOut(){
-    const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function() {
-      idToken = null;
-    });
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function() {
+    idToken = null;
+  });
+}
+
+async function formSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+
+  // Bootstrap client-side validation
+  if (form.checkValidity()) {
+    const formData = new FormData(form);
+    const response = await fetch('/register', {method: 'POST', body: formData});
+
+    // Server-side validation for username availability
+    if (response.status === 403) {
+      form.classList.remove('was-validated');
+      document.getElementById('username').classList.add('is-invalid');
+      document.getElementById('invalid-feedback').innerText = await response.text();
+    } else {
+      document.getElementById('username').classList.remove('is-invalid');
+      document.getElementById('username').classList.add('is-valid');
+      $('#registerModal').modal('hide');
+    }
+  } else {
+    form.classList.add('was-validated');
+  }
 }
