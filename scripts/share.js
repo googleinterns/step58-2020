19b1d5380a29ba61a99b6ea6aff3bff570d6ed6c
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-17 18:54:15 +0000

    Beginning stages of sharing features

---
+++ scripts/share.js
@@ -0,0 +1,20 @@
+const shareButton = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+const shareModal = document.querySelector('.share');
+
+const title = window.document.title;
+const url = window.document.location.href;
+
+shareButton.addEventLustener('click', () => {
+    if(navigator.share){
+        navigator.share({
+            title: `${tittle}`,
+            url: `${url}`
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        })
+        .catch(console.error);
+    } else {
+        overlay.classList.add('show-share')
+    }
+})
