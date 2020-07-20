cc57eade46fed8f7c07b5e9900ac322c21f08d83
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-20 19:21:30 +0000

    enforcing camelCase usage across our JavaScript files

--- scripts/share.js
+++ scripts/share.js
@@ -1,14 +1,14 @@
-var a2a_config = a2a_config || {};
-a2a_config.templates = a2a_config.templates || {};
+var a2aConfig = a2aConfig || {};
+a2aConfig.templates = a2aConfig.templates || {};
 
-a2a_config.templates.facebook = {
+a2aConfig.templates.facebook = {
     app_id: "5303202981",
     redirect_uri: "https://static.addtoany.com/menu/thanks.html",
     quote: "My Yellow Brick Code profile",
     hashtag: "learninghowtosoftwareengineer,coder,YellowBrickCode"
 };
 
-a2a_config.templates.twitter = {
+a2aConfig.templates.twitter = {
     text: "My Yellow Brick Code profile: ${title} ${link}",
     hashtags: "YellowBrickCode,coding,softwareengineering"
 };
