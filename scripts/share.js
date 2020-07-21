4ae8f066ee4937870743ad5541c8952e76497296
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-21 16:27:48 +0000

    Minor changes on share.js file from PR

--- scripts/share.js
+++ scripts/share.js
@@ -1,14 +1,14 @@
-var a2aConfig = a2aConfig || {};
+let a2aConfig = a2aConfig || {};
 a2aConfig.templates = a2aConfig.templates || {};
 
 a2aConfig.templates.facebook = {
-    app_id: "5303202981",
-    redirect_uri: "https://static.addtoany.com/menu/thanks.html",
-    quote: "My Yellow Brick Code profile",
-    hashtag: "learninghowtosoftwareengineer,coder,YellowBrickCode"
+    app_id: '5303202981',
+    redirect_uri: 'https://static.addtoany.com/menu/thanks.html',
+    quote: 'My Yellow Brick Code profile',
+    hashtag: 'learninghowtosoftwareengineer,coder,YellowBrickCode'
 };
 
 a2aConfig.templates.twitter = {
-    text: "My Yellow Brick Code profile: ${title} ${link}",
-    hashtags: "YellowBrickCode,coding,softwareengineering"
+    text: 'My Yellow Brick Code profile: ${title} ${link}',
+    hashtags: 'YellowBrickCode,coding,softwareengineering'
 };
