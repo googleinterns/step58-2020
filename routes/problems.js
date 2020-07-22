43f427a2531246cf4965ec3978b655721ccef888
Nicholas Sebastian Husin <40656571+Nicholas2750@users.noreply.github.com>
2020-07-22 10:51:30 -0500

    [Pod] Implement pagination for problem list (#50)
    * Add utility function to count Kind
    
    * Implement pagination in backend
    
    * Implement basic pagination in the frontend
    
    * Add equality helper to handlebars
    
    * Indicate currently active page
    
    Co-authored-by: Ife Omidiran <omidiran@college.harvard.edu>

--- routes/problems.js
+++ routes/problems.js
@@ -2,7 +2,9 @@
 
 const datastore = require('../modules/datastore.js');
 
-const PROBLEMS_KIND = "Problem";
+const PROBLEMS_KIND         = 'Problem';
+const DEFAULT_PAGE_SIZE     = 5;
+const DEFAULT_PAGE_INDEX    = 1;
 
 /**
  * @typedef {Object} Problem
@@ -18,14 +20,37 @@ class Problem {
   }
 }
 
+async function listProblems(pageIndex, pageSize) {
+  const startId = pageSize * (pageIndex - 1);
+  const query = datastore
+    .createQuery(PROBLEMS_KIND)
+    .filter('id', '>', parseInt(startId))
+    .order('id')
+    .limit(parseInt(pageSize));
+
+  return (await datastore.runQuery(query))[0];
+}
+
+
 module.exports = function(app) {
   app.get('/problems', async function(request, response) {
-    // TODO (@ifeomi) Post-MVP add limit to query and support pagination.
-    const query = datastore.createQuery(PROBLEMS_KIND);
-    const [entities] = await datastore.runQuery(query);
+    const clientPageIndex   = parseInt(request.query.pageIndex) || DEFAULT_PAGE_INDEX;
+    const clientPageSize    = parseInt(request.query.pageSize) || DEFAULT_PAGE_SIZE;
+
+    const entities = await listProblems(clientPageIndex, clientPageSize);
     const problems = entities.map((entity) => {
       return new Problem(entity.id, entity.title, entity.text);
     });
-    response.render('problems', {problems: problems});
+
+    const problemsCount     = await datastore.countKind(PROBLEMS_KIND);
+    const pageCount         = Math.ceil(problemsCount / clientPageSize);
+    const paginationArray   = Array.from(Array(pageCount), (_, i) => i + 1);
+
+    response.render('problems', {
+      problems: problems,
+      paginationArray: paginationArray,
+      currIndex: clientPageIndex
+    });
   });
 }
+
