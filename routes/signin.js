14e58f541c165fe62153e0716f1f886b6904b93c
Ife <omidiran@college.harvard.edu>
2020-07-27 16:59:48 -0500

    Integrate register and sign in routes with cookies

--- routes/signin.js
+++ routes/signin.js
@@ -2,17 +2,24 @@
 const auth = require('../modules/auth.js');
 
 module.exports = function(app) {
-  app.post('/sign-in', async function(request, response) {
+  app.get('/sign-in', async function(request, response) {
+    const token = request.cookies.token;
+    let userResponse;
     let user;
+
     try {
-      user = await auth.getUser(request.body.authToken);
+      userResponse = (await auth.getUser(token));
+      user = userResponse.user;
+      response.cookie('token', token, { expires: userResponse.expires});
     } catch(error) {
+      console.log(error);
       response.sendStatus(500);
     }
 
     if (!user.username) {
       response.status(200).send(user);
     } else {
+      response.cookie('username', user.username, {expires: userResponse.expires});
       response.status(203).send(user.username);
     }
   });
