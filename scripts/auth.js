fbfb5e5609fe8d378d7669f0bf3d6b47aa4949c9
Ife <omidiran@college.harvard.edu>
2020-07-27 17:03:29 -0500

    Modify client to store auth token as a cookie
    - Add helper functions for interacting cookies
    - Move client ID to script

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,18 +1,38 @@
-export let idToken         = null;
-
 const SIGNIN_LINK_ID       = 'signin-link'
 const SIGNOUT_LINK_ID      = 'signout-link';
 const PROFILE_CONTAINER_ID = 'profile-link';
+const DEFAULT_PATH = '/'
+const CLIENT_ID = '1044127873768-lqem54d3ke8hfanpakvb9f88ptlmton4.apps.googleusercontent.com';
 
-// Scope functions to global scope
-window.onSignIn = onSignIn;
-window.signOut = signOut;
+let auth2;
+
+window.addEventListener('load', function() {
+  const username = getCookieValue('username');
+  if (username) {
+    toggleNavLinks(username);
+  }
+});
+
+function init() {
+  gapi.load('auth2', function() {
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: CLIENT_ID,
+    });
+  });
+
+  if (!getCookieValue('token')) {
+    gapi.signin2.render(SIGNIN_LINK_ID, {
+      'onsuccess': onSignIn,
+    });
+  }
+}
 
 async function onSignIn(googleUser) {
-  idToken = googleUser.getAuthResponse().id_token;
-  const payload = new Blob([JSON.stringify({authToken: idToken})], {type: 'application/json'});
+  // expiration is set in the backend after verifying
+  setCookie('token', googleUser.getAuthResponse().id_token);
 
-  const response = await fetch('/sign-in', {method: 'POST', body: payload});
+  const response = await fetch('/sign-in');
 
   if (response.status === 203) {
     const username = await response.text();
@@ -26,12 +46,11 @@ async function onSignIn(googleUser) {
   }
 }
  
-function signOut() {
-  const auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function() {
-    idToken = null;
-    toggleNavLinks();
-  });
+async function signOut() {
+  await auth2.signOut();
+  deleteCookie('username');
+  deleteCookie('token');
+  toggleNavLinks();
 }
 
 async function formSubmit(event) {
@@ -52,6 +71,7 @@ async function formSubmit(event) {
       document.getElementById('username').classList.remove('is-invalid');
       document.getElementById('username').classList.add('is-valid');
       $('#registerModal').modal('hide');
+      toggleNavLinks(getCookieValue('username'));
     }
   } else {
     form.classList.add('was-validated');
@@ -69,8 +89,41 @@ function toggleNavLinks(username) {
     profileLink.href   = `/users/${username}`;
     profileLink.hidden = false;
   } else {
+    init();
     signInLink.hidden  = false;
     signOutLink.hidden = true;
     profileLink.hidden = true;
   }
+}
+
+/**
+ * Helper function to set a cookie value. If path is not provided,
+ * defaults to '/' (global).
+ * @param {string} key 
+ * @param {string} value 
+ * @param {string} path 
+ */
+function setCookie(key, value, path) {
+  path = path || DEFAULT_PATH;
+  document.cookie = `${key}=${value};path=${path}`
+}
+
+/**
+ * Helper function to delete a cookie.
+ * @param {string} key 
+ * @param {string} path 
+ */
+function deleteCookie(key, path) {
+  path = path || DEFAULT_PATH;
+  document.cookie = `${key}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=${path}`
+}
+
+/**
+ * Helper function to get value associated with a cookie.
+ * @param {string} key 
+ * @returns {string|undefined} 
+ */
+function getCookieValue(key) {
+  const cookie = document.cookie.split('; ').find(row => row.startsWith(key)); 
+  return cookie ? cookie.split('=')[1] : undefined;
 }
