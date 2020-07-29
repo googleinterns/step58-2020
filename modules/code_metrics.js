6eff416edf515d25f531c5d671996243e0238644
LaZshane <lazshane.hoskins@my.hamptonu.edu>
2020-07-29 17:45:01 +0000

    More metrics added: physical lines of code, number of parameters, and cyclomatic complexity density.

--- modules/code_metrics.js
+++ modules/code_metrics.js
@@ -10,11 +10,15 @@ function analyze(code) {
   const result = escomplex.analyse(code);
 
   // Round to 3 decimal places if needed
+  roundedDensity = +(result.aggregate.cyclomaticDensity.tofixed(3));
   roundedDifficulty = +(result.aggregate.halstead.difficulty.toFixed(3));
   return {
     cyclomatic: result.aggregate.cyclomatic,
     lloc: result.aggregate.sloc.logical,
-    difficulty: roundedDifficulty
+    difficulty: roundedDifficulty,
+    ploc: result.aggregate.sloc.physical,
+    params: result.aggregate.params,
+    density: roundedDensity
   };
 }
 
