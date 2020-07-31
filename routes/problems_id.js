0d75185e3563f925bccb6699f84cf949ce0965c5
Ife <omidiran@college.harvard.edu>
2020-07-31 13:17:20 -0500

    Remove getProblem helper function.

--- routes/problems_id.js
+++ routes/problems_id.js
@@ -13,20 +13,6 @@ const SOLUTION_KIND = 'Solution';
 problemUtil.addFromProblemsDir();
 
 /**
- * Gets problem according to the id given.
- * Utilizes parseInt() as integer sent through request
- * may have its typing wrongly inferred by Node.
- **/
-async function getProblem(id) {
-  const query = datastore
-    .createQuery(PROBLEMS_KIND)
-    .filter('id', '=', parseInt(id));
-
-  const [problems] = await datastore.runQuery(query);
-  return problems[0];
-}
-
-/**
  * Helper function to save submission of solution.
  * Fields in data are not nested to allow easier filtering.
  **/
@@ -50,7 +36,7 @@ module.exports = function(app) {
    * to the problem id given in the request parameter.
    **/
   app.get('/problems/:id', async function(request, response) {
-    const problem = await getProblem(request.params.id);
+    const problem = await datastore.getProblem(request.params.id);
     let dynamicContent = {
       'question-title'  : problem.title,
       'question-text'   : problem.text,
@@ -77,7 +63,7 @@ module.exports = function(app) {
     }
 
     const code      = request.body.code;
-    const problem   = (await getProblem(request.params.id));
+    const problem   = await datastore.getProblem(request.params.id);
     const tests     = problem.tests;
     const timeout   = problem.timeout;
 
